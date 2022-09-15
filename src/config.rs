05fb06095a7f5b3dc4fd08eabd36d62aae78a814
N8VENTURES <n8ventures@gmail.com>
2022-09-15 11:16:09 +0800

    Updated config.rs to hardcode Adobe location paths

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,8 @@
+use app_dirs::*;
+
+
+const APP_INFO: AppInfo = AppInfo{name: "CoreSync", author: "Adobe"};
+
 pub struct Config {
     /// Folder to search for font files in
     pub font_root_folder_path: String,
@@ -7,17 +12,22 @@ pub struct Config {
 
     /// Folder to save found Adobe fonts to
     pub target_folder_path: String,
+    
 }
 
 impl Config {
     pub(crate) fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() != 4 {
-            return Err("Config needs exactly 3 arguments");
+        if args.len() != 2 {
+            return Err(r#"You need to indicate a save location. ex. `afg "C:\'Font-grabbed"`"#);
         }
-
-        let font_root_folder_path = args[1].clone();
-        let entitlements_file_path = args[2].clone();
-        let target_folder_path = args[3].clone();
+        let root = get_app_dir(AppDataType::UserConfig, &APP_INFO, "plugins/livetype/r").unwrap();
+        let font_root_folder_path = root.display().to_string().to_owned();
+        let entitlements = get_app_dir(AppDataType::UserConfig, &APP_INFO, "plugins/livetype/c/").unwrap();
+        let  mut entitlements_file_path = entitlements.display().to_string().to_owned();
+        let en_xml = "/entitlements.xml".to_string();
+        let target_folder_path = args[1].clone();
+        
+        entitlements_file_path.push_str(&en_xml);
 
         Ok(Config {
             font_root_folder_path,
