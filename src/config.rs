313c10a74239f491fe111af5df37f428c80ba5d5
N8VENTURES <n8ventures@gmail.com>
2022-09-22 03:17:53 +0800

    typo fix

--- src/config.rs
+++ src/config.rs
@@ -18,13 +18,15 @@ pub struct Config {
 impl Config {
     pub(crate) fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() != 2 {
-            return Err(r#"You need to indicate a save location. ex. `afg "C:\'Font-grabbed"`"#);
+            return Err(r#"You need to indicate a save location. ex. `afg "C:\'Fonts-grabbed"`"#);
         }
         let root = get_app_dir(AppDataType::UserConfig, &APP_INFO, "plugins/livetype/r").unwrap();
         let font_root_folder_path = root.display().to_string().to_owned();
+
         let entitlements = get_app_dir(AppDataType::UserConfig, &APP_INFO, "plugins/livetype/c/").unwrap();
         let  mut entitlements_file_path = entitlements.display().to_string().to_owned();
         let en_xml = "/entitlements.xml".to_string();
+
         let target_folder_path = args[1].clone();
         
         entitlements_file_path.push_str(&en_xml);
